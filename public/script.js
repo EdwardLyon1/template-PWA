4fd0b2863e8a7cc5b8b12aace700e0aad0ba6975
EdwardLyon1 <165915866+EdwardLyon1@users.noreply.github.com>
2024-04-03 16:05:19 +0200

    Delete public directory

--- public/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/visitorCount')
-        .then(response => response.json())
-        .then(data => {
-            const countDisplay = document.getElementById('visitorCount');
-            countDisplay.textContent = data.visitorCount;
-        })
-        .catch(error => console.error('Error fetching visitor count:', error));
-});
